429eee29c8e4f7d23784fbcc0954288539287a03
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-04-01 21:57:29 -0400

    Slideshow play/pause functions

--- thisExercise.js
+++ thisExercise.js
@@ -1,48 +1,57 @@
 
 //Create a single object named `slideshow` that represents the data and functionality of a picture slideshow
-var slideShow=
-{
-//An array called `photoList` that contains the names of the photos as strings
-    photoList: ['photo1', 'photo2', 'photo3','photo4', 'photo5'],
+var slideshow = {
+    photoList: ['photo1', 'photo2', 'photo3', 'photo4','photo5'],
     
 //An integer `currentPhotoIndex` that represents which photo in the `photoList` is currently displayed
-    currentPhotoIndex: 0, //0='photo'1
-
-//A function `getCurrentPhoto()` that returns the current photo from the list
-    getCurrentPhoto: function()
-    {
-		console.log("The current slide is: " + this.photoList[this.currentPhotoIndex]);
-    },
+    currentPhotoIndex: 0,
 
 //A `nextPhoto()` function that moves `currentPhotoIndex` to the next index `if `there is one
-    nextPhoto:function()
-    {
-        if(this.currentPhotoIndex < (this.photoList.length-1))
-        {
-		this.currentPhotoIndex++;
-		console.log("The next slide is: " + this.photoList[this.currentPhotoIndex]);
-		}else
-		console.log("End of slideshow");
+    nextPhoto: function() {
+        if(this.currentPhotoIndex < this.photoList.length - 1) {
+            this.currentPhotoIndex++;
+            console.log('currentPhoto is: '+ this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('End of Slideshow');
+              this.pause();
+        }
     },
 
 //A `prevPhoto()` function that does the same thing, but backwards
-    previousPhoto:function()
-    {
-        if(this.currentPhotoIndex>0)
-        {
-		this.currentPhotoIndex--;
-		console.log("Previous photo is: "+this.photoList[this.currentPhotoIndex]);
-		}else
-		console.log("Start of slideshow");
+    prevPhoto: function() {
+        if(this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            console.log('currentPhoto is: ' + this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('Start of Slideshow');
+        }
+    },
+
+//A function `getCurrentPhoto()` that returns the current photo from the list
+    getCurrentPhoto: function() {
+        return this.photoList[this.currentPhotoIndex];  
+    },
+
+//create an empty property named playInterval
+    playInterval: null,
+
+//play() function that moves to the next photo ever 2000ms until the end    
+    play: function() {
+        var self = this;
+        this.playInterval = setInterval(function(){self.nextPhoto()}, 2000)
+    },
+
+//pause() function that stops the slideshow    
+    pause: function() {
+        clearInterval(this.playInterval);
+        }
     }
-}
-
-
-
-slideShow.previousPhoto();
-slideShow.getCurrentPhoto();
-slideShow.nextPhoto();
-slideShow.nextPhoto();
-slideShow.nextPhoto();
-slideShow.nextPhoto();
-slideShow.nextPhoto();
+slideshow.play()
+//slideshow.pause();
+//slideShow.previousPhoto();
+//slideShow.getCurrentPhoto();
+//slideShow.nextPhoto();
+//slideShow.nextPhoto();
+//slideShow.nextPhoto();
+//slideShow.nextPhoto();
+//slideShow.nextPhoto();
